52dba34fcbcb701abafdfc5c09ab4c247af01ee8
PaoLuga <theo.gedin@gmail.com>
2025-04-17 00:56:07 +0200

    fix: use &#39; instead of &apos; for JSX apostrophe to resolve react/no-unescaped-entities lint error

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import MainLayout from "../layouts/MainLayout";
 import { useState } from "react";
 import ContactForm from "../components/ContactForm";
